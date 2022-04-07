aaa9a71f0e1a7ec5d4a3f1f026a8bc8cec5b799d
Daven Young <davenyoung@msn.com>
2022-04-07 10:57:44 -0600

    commit tree e60bd9ced32b5e8c6486d23cd92dfa6c19c2c9d7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superbowlWin(record) {
+    let answer = record.find(function (game) {
+        return game.result ==="W";
+    });
+    if (answer){
+    return answer.year;
+    }
+}
